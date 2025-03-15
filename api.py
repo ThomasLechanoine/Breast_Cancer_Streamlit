8b75180789f5c5dea4e611da6d31b97b697cbbeb
Florent Coulon <florent.coulon@lifttheweb.com>
2025-03-15 14:58:33 +0100

    préparation mise en production streamlit

---
+++ api.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+params = {
+    'vs_currency': 'usd',
+    'days': '30',  # Nombre de jours de données historiques
+    'interval': 'daily'  # Intervalle des données (daily, weekly, etc.)
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+# Convertir en DataFrame pour une manipulation facile
+df = pd.DataFrame(data['prices'], columns=['timestamp', 'price'])
